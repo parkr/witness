da4ff3128725465758850fb556159a23d8d51a63
Parker Moore <parkrmoore@gmail.com>
2013-05-19 16:35:25 +0200

    fix for firefox time mouseovers

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -1,20 +1,20 @@
 (function(d){
     var showXML = function(){
-        this.innerText = this.dataset.xmlDate;
+        this.textContent = this.dataset.xmlDate;
     };
     var showFrom = function(){
-        this.innerText = this.dataset.fromDate;
+        this.textContent = this.dataset.fromDate;
     };
 
     var updateTimes = function(){
         var els = d.getElementsByClassName("time");
         for(var i=0; i<els.length; i++){
-            els[i].dataset.xmlDate = els[i].innerText;
+            els[i].dataset.xmlDate = els[i].textContent;
             els[i].dataset.fromDate = moment(els[i].innerText).fromNow();
-            els[i].innerText = els[i].dataset.fromDate;
             els[i].onmouseover = showXML;
             els[i].onmouseout  = showFrom;
             els[i].onmouseup   = showFrom;
+            els[i].textContent = els[i].dataset.fromDate;
         }
     };
 
